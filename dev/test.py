d00555c04db07217eaa909731d997dc2e590c9f7
romainsacchi <r_s@me.com>
2025-03-20 08:51:07 +0100

    Update test.py.

--- dev/test.py
+++ dev/test.py
@@ -1,43 +1,49 @@
-from windwhisper import windturbines
+from windwhisper import WindTurbines, NoisePropagation, NoiseAnalysis
 import xarray as xr
 
 # we can preload the wind speed data, otherwise, the tool will do it every time
 filepath_wind_speed = "/Users/romain/GitHub/windwhisper/dev/fixtures/era5_mean_2013-2022_month_by_hour.nc"
 filepath_correction = "/Users/romain/GitHub/windwhisper/dev/fixtures/ratio_gwa2_era5.nc"
 
-
 def wind_speed_data():
+
     wind_speed = xr.open_dataset(filepath_wind_speed).to_array().mean(dim="month")
     correction = xr.open_dataset(filepath_correction).to_array()
-    correction = correction.sel(variable='ratio_gwa2_era5_mean_WS').interp(
-        latitude=wind_speed.latitude,
-       longitude=wind_speed.longitude,
-       method="linear"
-    )
+    correction = correction.sel(variable='ratio_gwa2_era5_mean_WS').interp(latitude=wind_speed.latitude, longitude=wind_speed.longitude, method="linear")
     return wind_speed * correction
 
-
-data = wind_speed_data()
+wind_speed_data = wind_speed_data()
 
 wind_turbines = {
     'Turbine 0':
-        {'diameter': 70.0,
-         'hub height': 85.0,
-         'position': (43.67402852737928, 7.2169801653160395),
-         'power': 2500.0},
-    # 'Turbine 1':
-    #     {'diameter': 60.0,
-    #      'hub height': 55.0,
-    #      'position': (43.678986127617584, 7.222049102839316),
-    #      'power': 1500.0},
-    # 'Turbine 2':
-    #     {'diameter': 60.0,
-    #      'hub height': 55.0,
-    #      'position': (43.67262338001209, 7.217452048839465),
-    #      'power': 2500.0},
+     {
+        'diameter': 70.0,
+        'hub height': 85.0,
+        'position': (43.45111343125036, 5.2518247370645215),
+        'power': 2500.0
+     },
 }
 
-wt = windturbines.WindTurbines(
+wt = WindTurbines(
     wind_turbines=wind_turbines,
-    wind_speed_data=data,
+    wind_speed_data=wind_speed_data,
+    #retrain_model=True
+)
+
+# elevation_data = xr.open_dataset("fixtures/Copernicus_DSM_90m_COG.nc")
+
+noise_prop = NoisePropagation(
+    wind_turbines=wt.wind_turbines,
+    humidity=70,
+    temperature=20,
+    #elevation_data=elevation_data,
+)
+
+noise_analysis = NoiseAnalysis(
+    noise_propagation=noise_prop,
+    wind_turbines=wt.wind_turbines,
+)
+
+noise_analysis.generate_map(
+    filepath="my_noise_map.html"
 )
