1c2170b42095932913f1c0822edb4b93230e311f
lenakaufmann <kaufmannl@student.ethz.ch>
2025-06-26 11:20:55 +0200

    I added the factor for lifetime of a wind turbin

--- windwhisper/health_impacts.py
+++ windwhisper/health_impacts.py
@@ -15,7 +15,6 @@ population_df = pd.read_csv("data/Population_data.csv", sep = ";")
 # unify column names
 disease_df.columns = disease_df.columns.str.lower()
 population_df.columns = population_df.columns.str.lower()
-print("Spaltennamen in population_df:", population_df.columns.tolist())
 
 
 def get_disease_totals(country, disease, disease_df):
@@ -36,7 +35,7 @@ def get_population_totals(country, population_df):
     return float(row['population'].values[0])
 
 # 1. Highly Annoyed
-def calculate_highly_annoyed_dalys(exposed_individuals, lden, noise_type_ha):
+def calculate_highly_annoyed_dalys(exposed_individuals, lden, lifetime, noise_type_ha):
     p = params['highly_annoyed'][noise_type_ha]
     a = p['a']
     b = p['b']
@@ -45,11 +44,12 @@ def calculate_highly_annoyed_dalys(exposed_individuals, lden, noise_type_ha):
     percentage_affected = a + b * lden + c * (lden ** 2)
     affected = exposed_individuals * (percentage_affected / 100)
     yld = affected * disability_weight
-    dalys = yld
+    dalys_per_year = yld
+    dalys = dalys_per_year * lifetime
     return dalys
 
 # 2. High Sleep Disorder
-def calculate_high_sleep_disorder_dalys(exposed_individuals, lnight, noise_type_hsd):
+def calculate_high_sleep_disorder_dalys(exposed_individuals, lnight, lifetime, noise_type_hsd):
     p = params['high_sleep_disorder'][noise_type_hsd]
     a = p['a']
     b = p['b']
@@ -58,11 +58,12 @@ def calculate_high_sleep_disorder_dalys(exposed_individuals, lnight, noise_type_
     percentage_affected = a + b * lnight + c * (lnight ** 2)
     affected = exposed_individuals * (percentage_affected / 100)
     yld = affected * disability_weight
-    dalys = yld
+    dalys_per_year = yld
+    dalys = dalys_per_year * lifetime
     return dalys
 
 # 3. Ischemic Heart Disease (IHD)
-def calculate_ihd_dalys(exposed_individuals, lden, noise_type, country):
+def calculate_ihd_dalys(exposed_individuals, lden, lifetime, noise_type, country):
     p = params['ischemic_heart_disease'][noise_type]
     rr_per_10db = p['a']
     threshold = p['threshold']
@@ -75,11 +76,12 @@ def calculate_ihd_dalys(exposed_individuals, lden, noise_type, country):
     yld_total, yll_total = get_disease_totals(country, "ischemic_heart_disease", disease_df)
     yld_noise = yld_total * paf
     yll_noise = yll_total * paf
-    dalys = yld_noise + yll_noise
+    dalys_per_year = yld_noise + yll_noise
+    dalys = dalys_per_year * lifetime
     return dalys
 
 # 4. Diabetes
-def calculate_diabetes_dalys(exposed_individuals, lden, noise_type, country):
+def calculate_diabetes_dalys(exposed_individuals, lden, lifetime, noise_type, country):
     p = params['diabetes'][noise_type]
     rr_per_10db = p['a']
     threshold = p['threshold']
@@ -92,10 +94,11 @@ def calculate_diabetes_dalys(exposed_individuals, lden, noise_type, country):
     yld_total, yll_total = get_disease_totals(country, "diabetes", disease_df)
     yld_noise = yld_total * paf
     yll_noise = yll_total * paf
-    dalys = yld_noise + yll_noise
+    dalys_per_year = yld_noise + yll_noise
+    dalys = dalys_per_year * lifetime
     return dalys
 # 5. Stroke
-def calculate_stroke_dalys(exposed_individuals, lden, noise_type, country):
+def calculate_stroke_dalys(exposed_individuals, lden, lifetime, noise_type, country):
     p = params['stroke'][noise_type]
     rr_per_10db = p['a']
     threshold = p['threshold']
@@ -108,15 +111,16 @@ def calculate_stroke_dalys(exposed_individuals, lden, noise_type, country):
     yld_total, yll_total = get_disease_totals(country, "stroke", disease_df)
     yld_noise = yld_total * paf
     yll_noise = yll_total * paf
-    dalys = yld_noise + yll_noise
+    dalys_per_year = yld_noise + yll_noise
+    dalys = dalys_per_year * lifetime
     return dalys
 
-def calculate_total_dalys(exposed_individuals, lden, lnight,  noise_type_ha, noise_type_hsd, noise_type, country):
-    dalys_ha = calculate_highly_annoyed_dalys(exposed_individuals, lden, noise_type_ha)
-    dalys_hsd = calculate_high_sleep_disorder_dalys(exposed_individuals, lnight, noise_type_hsd)
-    dalys_ihd = calculate_ihd_dalys(exposed_individuals, lden, noise_type, country)
-    dalys_diabetes = calculate_diabetes_dalys(exposed_individuals, lden, noise_type, country)
-    dalys_stroke = calculate_stroke_dalys(exposed_individuals, lden, noise_type, country)
+def calculate_total_dalys(exposed_individuals, lden, lnight, lifetime,  noise_type_ha, noise_type_hsd, noise_type, country):
+    dalys_ha = calculate_highly_annoyed_dalys(exposed_individuals, lden, lifetime, noise_type_ha)
+    dalys_hsd = calculate_high_sleep_disorder_dalys(exposed_individuals, lnight, lifetime, noise_type_hsd)
+    dalys_ihd = calculate_ihd_dalys(exposed_individuals, lden, lifetime, noise_type, country)
+    dalys_diabetes = calculate_diabetes_dalys(exposed_individuals, lden, lifetime, noise_type, country)
+    dalys_stroke = calculate_stroke_dalys(exposed_individuals, lden, lifetime, noise_type, country)
     total_dalys = dalys_ha + dalys_hsd + dalys_ihd + dalys_diabetes + dalys_stroke
 
     return total_dalys
@@ -129,11 +133,16 @@ if __name__ == "__main__":
     noise_type_hsd = "combined"
     noise_type = "road middle"
     exposed = 20000
+    lifetime = 20
 
-    print("Highly Annoyed:", calculate_highly_annoyed_dalys(exposed, lden, noise_type_ha))
-    print("Sleep Disorder:", calculate_high_sleep_disorder_dalys(exposed, lnight, noise_type_hsd))
-    print("IHD:", calculate_ihd_dalys(exposed, lden, noise_type, country))
-    print("Diabetes:", calculate_diabetes_dalys(exposed, lden, noise_type, country))
-    print("Stroke:", calculate_stroke_dalys(exposed, lden, noise_type, country))
-    print("Total", calculate_total_dalys(exposed, lden, lnight,  noise_type_ha, noise_type_hsd, noise_type, country))
+    print("Country:", country)
+    print("Noise Level (Lden, Lnight):", lden,"dB(A)",",", lnight,"dB(A)",)
+    print("Exposed Population:", exposed, "people")
+    print("Lifetime of the wind turbine:", lifetime, "years")
+    print("Highly Annoyed:", calculate_highly_annoyed_dalys(exposed, lden, lifetime, noise_type_ha),"DALYs")
+    print("Sleep Disorder:", calculate_high_sleep_disorder_dalys(exposed, lnight, lifetime, noise_type_hsd),"DALYs")
+    print("IHD:", calculate_ihd_dalys(exposed, lden, lifetime, noise_type, country),"DALYs")
+    print("Diabetes:", calculate_diabetes_dalys(exposed, lden, lifetime, noise_type, country),"DALYs")
+    print("Stroke:", calculate_stroke_dalys(exposed, lden, lifetime, noise_type, country),"DALYs")
+    print("Total:", calculate_total_dalys(exposed, lden, lnight, lifetime, noise_type_ha, noise_type_hsd, noise_type, country),"DALYs")
 
